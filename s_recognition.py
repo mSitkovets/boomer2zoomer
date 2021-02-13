f6de6fa84b86de6d227cf64e35d8eb7e916c855c
Joanna Diao <joannadiao@hotmail.com>
2021-02-13 17:21:39 -0500

    added IBM speech recognition

--- s_recognition.py
+++ s_recognition.py
@@ -2,13 +2,24 @@ import requests
 import os
 from dotenv import load_dotenv, find_dotenv
 import speech_recognition as sr
+from ibm_watson import SpeechToTextV1
+import json
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+load_dotenv(find_dotenv())
 
 r = sr.Recognizer()
+speech = sr.Microphone()
+authenticator = IAMAuthenticator(os.environ.get("IBMAPI_KEY"))
+speech_to_text = SpeechToTextV1(
+    authenticator=authenticator
+)
 
-with sr.Microphone() as source:
-    # read the audio data from the default microphone
-    audio_data = r.record(source, duration=5)
-    print("Recognizing...")
-    # convert speech to text
-    text = r.recognize_google(audio_data)
-    print(text)
+speech_to_text.set_service_url(os.environ.get("IBM_URL"))
+
+with speech as source:
+    print("say something!!…")
+    audio_file = r.adjust_for_ambient_noise(source)
+    audio_file = r.listen(source)
+speech_recognition_results = speech_to_text.recognize(audio=audio_file.get_wav_data(), content_type='audio/wav').get_result()
+print(json.dumps(speech_recognition_results, indent=2))
